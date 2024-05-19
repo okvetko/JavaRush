25e1121422eba9d9be58b0d32d128a0de9cf6ac2
okvetko <kvetko.olga@gmail.com>
2024-05-19 23:45:56 +0300

    feat(JavaRush): methods

---
+++ src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String a = new String("a");
+        String b = new String("b");
+        String c = new String("c");
+        String d = new String("d");
+        String e = new String("e");
+        String f = new String("f");
+        String g = new String("h");
+        String h = new String("h");
+        String i = new String("i");
+        String j = new String("j");
+        int[] data1 = new int[1];
+        int[] data2 = new int[2];
+        int[] data3 = new int[3];
+        int[] data4 = new int[4];
+        int[] data5 = new int[5];
+        Scanner console = new Scanner(System.in);
+        Scanner console2 = new Scanner(System.in);
+    }
+}
