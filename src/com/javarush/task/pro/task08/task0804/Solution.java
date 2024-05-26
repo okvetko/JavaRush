8693761cd796378be76a06bf550193e506c6988a
okvetko <kvetko.olga@gmail.com>
2024-05-26 23:28:00 +0300

    feat(JavaRush): method Math

---
+++ src/com/javarush/task/pro/task08/task0804/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task08.task0804;
+
+/* 
+Randomizer
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(generateNumber());
+
+            }
+
+    public int generateNumber() {
+
+
+     return (int) (Math.random() * 99) + 1;
+    }
+}
