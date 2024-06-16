2e79945da44f24bbe35bcef33028aa6ccf34bb01
okvetko <kvetko.olga@gmail.com>
2024-06-17 00:26:11 +0300

    feat(JavaRush): literals

---
+++ src/com/javarush/task/pro/task09/task0902/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task09.task0902;
+
+/* 
+Литералы с плавающей точкой
+*/
+
+public class Solution {
+    public int a = 0.;
+    public int b = .0;
+    public int c = 100D;
+    public int d = 100.0;
+    public int e = 1.11E5;
+    public int f = 200F;
+    public int g = 0.F;
+}
