68b6231fb3c8f508bd3ef21ca5eadbc485a1ca2a
okvetko <kvetko.olga@gmail.com>
2024-08-12 01:08:41 +0300

    feat(JavaRush): super

---
+++ src/com/javarush/task/pro/task10/task1006/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task10.task1006;
+
+/* 
+Кто тут наследник?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ElectricCar car3 = new ElectricCar();
+        GasCar car2 = new GasCar();
+        HybridCar car1 = new HybridCar();
+
+    }
+}
