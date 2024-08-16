0db1c3f322ec38e6e2bf72ad3bb8ab36f01b8ac1
okvetko <kvetko.olga@gmail.com>
2024-08-16 22:56:48 +0300

    feat(JavaRush): Defragmentation

---
+++ src/com/javarush/task/pro/task10/task1012/Memory.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task10.task1012;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/* 
+Дефрагментация памяти
+*/
+
+public class Memory {
+
+    public static void main(String[] args) {
+        String[] memory = {"object15", null, null, "object2", null, null, null, "object32", null, "object4"};
+        executeDefragmentation(memory);
+        System.out.println(Arrays.toString(memory));
+    }
+
+    public static void executeDefragmentation(String[] array) {
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] != null){
+               array[index] = array[i];
+               index++;
+            }
+        }
+        while (index< array.length){
+            array[index] = null;
+            index++;
+        }
+
+    }
+}
