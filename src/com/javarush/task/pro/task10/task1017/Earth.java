4bf4282b0c20394db7703bd1b6ad6a307e850fc0
okvetko <kvetko.olga@gmail.com>
2024-08-20 00:23:52 +0300

    feat(JavaRush): class+method

---
+++ src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+        Africa africa = new Africa(1000);
+        Antarctica antarctica = new Antarctica(10);
+        Australia australia = new Australia(500);
+        Eurasia eurasia = new Eurasia(600);
+        NorthAmerica northAmerica = new NorthAmerica(650);
+        SouthAmerica southAmerica = new SouthAmerica(660);
+    }
+}
