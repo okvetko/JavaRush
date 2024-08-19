53867c3fd2a54470a9b3efe147e13c775307a60f
okvetko <kvetko.olga@gmail.com>
2024-08-20 00:48:59 +0300

    feat(JavaRush): get+set

---
+++ src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+    Mouse mouse = new Mouse();
+    Keyboard keyboard = new Keyboard();
+    Monitor monitor = new Monitor();
+    SystemUnit systemUnit = new SystemUnit();
+
+}
---
+++ src/com/javarush/task/pro/task10/task1021/Car.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task10.task1021;
+
+/* 
+Автопроизводитель
+*/
+
+public class Car {
+    private String model;
+    private int year;
+    private String color;
+
+    public void initialize (String a, int b, String co){
+        this.model = a;
+        this.year = b;
+        this.color = co;
+    }
+
+}
